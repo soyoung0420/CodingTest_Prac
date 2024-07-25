af1d1261957d3386b06985eef9fd484e51bab63a
Younglog <126383032+soyoung0420@users.noreply.github.com>
2024-07-25 17:35:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.76 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        
+       String[] strArr = s.split(" ", -1);
+        
+        StringBuilder result = new StringBuilder();
+              
+        for(int i = 0; i < strArr.length; i++){
+            String word = strArr[i];
+            StringBuilder newWord = new StringBuilder();
+            for(int j = 0; j < word.length(); j++){
+                 char ch = word.charAt(j);
+                
+                if(j == 0){
+                    if(ch < '0' || ch > '9'){
+                       newWord.append(Character.toUpperCase(ch));   
+                    } else {
+                        newWord.append(ch);
+                    }           
+                } else {
+                    newWord.append(Character.toLowerCase(ch));
+                }
+                        
+            }
+            result.append(newWord);
+            if(i < strArr.length-1){
+                result.append(" ");
+            }
+        }
+        String answer = result.toString();
+        System.out.print(answer);
+        return answer;
+    }
+}
