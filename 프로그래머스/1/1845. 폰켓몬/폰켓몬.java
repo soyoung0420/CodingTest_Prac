7b185180300887dc5f86ff41132874b26e1e74f3
Younglog <126383032+soyoung0420@users.noreply.github.com>
2024-07-27 20:16:32 +0900

    [level 1] Title: 폰켓몬, Time: 4.51 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> pocket = new HashSet<>();
+        for(int num : nums){
+            pocket.add(num);
+        }
+
+        if(pocket.size()>(nums.length/2)){
+            return (nums.length/2);
+        }else {
+            return pocket.size();
+        }    
+    }
+}
