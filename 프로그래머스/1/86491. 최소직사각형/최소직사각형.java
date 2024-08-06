746bf8838ad42dd66cf0692720aeaff940ca67a9
Younglog <126383032+soyoung0420@users.noreply.github.com>
2024-08-06 22:51:07 +0900

    [level 1] Title: 최소직사각형, Time: 1.91 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,21 +3,22 @@ import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
         
+        // 큰 값중에서 큰값과 작은 값중에서 큰값을 담을 변수 선언
         int maxMax = 0;
         int minMax = 0;
-        
-       for(int[] size : sizes){
+       
+        for(int i =0 ; i < sizes.length; i++){
+           // 명함의 가로와 세로중 가장 긴변을 max에 담고, 가장 짧은 변을 min에 담는다. 
+           int max = Math.max(sizes[i][0], sizes[i][1]);
+           int min = Math.min(sizes[i][0], sizes[i][1]);
            
-           int max = Math.max(size[0], size[1]);
-           int min = Math.min(size[0], size[1]);
-           
+           // 가장 긴변끼리 비교했을 때 가장 긴변, 짧은 변 중에서 가장 긴 변을 찾기
            maxMax = Math.max(maxMax, max);
-           minMax = Math.max(minMax, min);
-           
-       }
-        
+           minMax = Math.max(minMax, min);     
+        }
+      
+        // 그때에 두 길이의 곱이 모든 명함을 포함하게 된다. 
         return maxMax * minMax;
     }
-
 }
 
