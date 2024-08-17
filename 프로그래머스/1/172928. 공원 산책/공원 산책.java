18dc85050bb5c686db206fa890054b9376f70ebd
Younglog <126383032+soyoung0420@users.noreply.github.com>
2024-08-17 23:02:40 +0900

    [level 1] Title: 공원 산책, Time: 0.29 ms, Memory: 89.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.java
@@ -0,0 +1,66 @@
+class Solution {
+    public int[] solution(String[] park, String[] routes) {
+        int x = 0; 
+        int y = 0;
+
+        for(int i = 0; i < park.length; i++) {
+            for(int j = 0; j < park[i].length(); j++) {
+                if(park[i].charAt(j) == 'S') {
+                    x = i; 
+                    y = j;
+                    break;
+                }
+            }
+        }
+       
+        for(String route : routes) {
+            char direction = route.charAt(0);
+            int steps = route.charAt(2) - '0';
+
+            int newX = x;
+            int newY = y;
+
+            switch(direction) {
+                case 'E': newY += steps; break;
+                case 'W': newY -= steps; break;
+                case 'N': newX -= steps; break;
+                case 'S': newX += steps; break;
+            }
+
+           boolean isBlocked = false;
+    int tempX = x;
+    int tempY = y;
+
+    // 이동 중 경로 확인
+    for (int i = 1; i <= steps; i++) {
+        switch(direction) {
+            case 'E': tempY++; break;
+            case 'W': tempY--; break;
+            case 'N': tempX--; break;
+            case 'S': tempX++; break;
+        }
+
+        // 경계를 벗어나는지 확인
+        if (tempX < 0 || tempX >= park.length || tempY < 0 || tempY >= park[0].length()) {
+            isBlocked = true;
+            break;
+        }
+
+        // 장애물이 있는지 확인
+        if (park[tempX].charAt(tempY) == 'X') {
+            isBlocked = true;
+            break;
+        }
+    }
+
+    if (!isBlocked) {
+        x = newX;
+        y = newY;
+    }
+        }
+      
+        int[] answer = {x, y};
+        return answer;
+    }
+         
+}
