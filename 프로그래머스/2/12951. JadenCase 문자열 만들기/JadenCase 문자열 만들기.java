1fa1fd9d8c9c1faffe07aff6a3f3b9b55424e5eb
Younglog <126383032+soyoung0420@users.noreply.github.com>
2025-04-16 23:50:34 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.18 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,36 +1,25 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     public String solution(String s) {
-        
-       String[] strArr = s.split(" ",-1);
-        
-        StringBuilder result = new StringBuilder();
-              
-        for(int i = 0; i < strArr.length; i++){
-            String word = strArr[i];
-            StringBuilder newWord = new StringBuilder();
-            for(int j = 0; j < word.length(); j++){
-                 char ch = word.charAt(j);
-                
-                if(j == 0){
-                    if(ch < '0' || ch > '9'){
-                       newWord.append(Character.toUpperCase(ch));   
-                    } else {
-                        newWord.append(ch);
-                    }           
-                } else {
-                    newWord.append(Character.toLowerCase(ch));
-                }
-                        
-            }
-            result.append(newWord);
-            if(i < strArr.length-1){
-                result.append(" ");
-            }
+         String[] words = s.toLowerCase().split(" ", -1);
+         StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < words.length; i++) {
+        String word = words[i];
+        if (word.length() == 0) {
+            sb.append("");
+        } else {
+            sb.append(Character.toUpperCase(word.charAt(0)));
+            sb.append(word.substring(1));
         }
-        String answer = result.toString();
-        System.out.print(answer);
-        return answer;
+
+        if (i < words.length - 1) {
+            sb.append(" ");
+        }
+    }
+
+        return sb.toString();
     }
 }
